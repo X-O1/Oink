fe23be2dd277763de96c70bc7ec45fff30cb1129
X-O1 <series-x-01@protonmail.com>
2024-02-05 14:51:41 -0800

    Added unit tests for beneficiaries withdrawing funds from their decendant's account

--- test/unit/OinkTest.t.sol
+++ test/unit/OinkTest.t.sol
@@ -3,20 +3,19 @@ pragma solidity ^0.8.18;
 
 import {Test, console} from "forge-std/Test.sol";
 import {Oink} from "../../src/Oink.sol";
-import {OinkCoin, OinkNft} from "../../src/mocks/MockAssets.m.sol";
+import {OinkCoin} from "../../src/mocks/MockAssets.m.sol";
 import {DeployOink} from "../../script/DeployOink.s.sol";
-import {DeployOinkCoin, DeployOinkNft} from "../../script/DeployMockAssets.s.sol";
+import {DeployOinkCoin} from "../../script/DeployMockAssets.s.sol";
 
 contract OinkTest is Test {
     Oink oink;
     OinkCoin oinkCoin;
-    OinkNft oinkNft;
 
+    address OinkContract = 0x90193C961A926261B756D1E5bb255e67ff9498A1;
     address USER = makeAddr("user");
     address BENEFICIARY = makeAddr("beneficiary");
     address BENEFICIARY2 = makeAddr("beneficiary2");
     address OinkCoinAddress = 0xA8452Ec99ce0C64f20701dB7dD3abDb607c00496;
-    address OinkNftAddress = 0xBb2180ebd78ce97360503434eD37fcf4a1Df61c3;
     address UserAddrses = 0x6CA6d1e2D5347Bfab1d91e883F1915560e09129D;
 
     function setUp() external {
@@ -26,22 +25,21 @@ contract OinkTest is Test {
         DeployOinkCoin deployOinkCoin = new DeployOinkCoin();
         oinkCoin = deployOinkCoin.run();
 
-        DeployOinkNft deployOinkNft = new DeployOinkNft();
-        oinkNft = deployOinkNft.run();
-
+        vm.deal(OinkContract, 10 ether);
+        vm.deal(USER, 10 ether);
         vm.deal(USER, 10 ether);
         vm.deal(BENEFICIARY, 10 ether);
         vm.deal(BENEFICIARY2, 10 ether);
     }
 
-    function testUserEtherDepositAndThatBalancesUpdate() public {
+    function testEtherDepositAndThatBalancesUpdate() public {
         vm.prank(USER);
         oink.depositEther(1 ether);
 
         assertEq(oink.getEtherBalance(USER), 1 ether);
     }
 
-    function testUserErc20DepositAndThatBalancesUpdate() public {
+    function testErc20DepositAndThatBalancesUpdate() public {
         vm.prank(USER);
         oinkCoin.approve(address(oink), 10);
         vm.prank(USER);
@@ -50,13 +48,34 @@ contract OinkTest is Test {
         assertEq(oink.getErc20Balance(USER, OinkCoinAddress), 10);
     }
 
-    function testUserNftDepositAndThatBalancesUpdate() public {
+    function testEtherWithdrawlAndBalancesUpdate() public {
         vm.prank(USER);
-        oinkNft.approve(address(oink), 1);
-        vm.prank(USER);
-        oink.depositNft(OinkNftAddress, 1);
+        oink.depositEther(5 ether);
 
-        assertEq(oink.getNftBalance(USER, OinkNftAddress), 1);
+        vm.prank(USER);
+        oink.withdrawEther(1 ether);
+
+        assertEq(oink.getEtherBalance(USER), 4 ether);
+
+        vm.expectRevert();
+        vm.prank(USER);
+        oink.withdrawEther(6 ether);
+    }
+
+    function testErc20WithdrawlAndBalancesUpdate() public {
+        vm.prank(USER);
+        oinkCoin.approve(address(oink), 10);
+        vm.prank(USER);
+        oink.depositErc20(OinkCoinAddress, 10);
+
+        vm.prank(USER);
+        oink.withdrawErc20(OinkCoinAddress, 5);
+
+        assertEq(oink.getErc20Balance(USER, OinkCoinAddress), 5);
+
+        vm.expectRevert();
+        vm.prank(USER);
+        oink.withdrawErc20(OinkCoinAddress, 10);
     }
 
     function testUserAddingBeneficiaries() public {
@@ -66,6 +85,38 @@ contract OinkTest is Test {
         oink.addBeneficiary(BENEFICIARY2);
         vm.prank(USER);
         oink.getListOfBeneficiaries(USER);
+    }
+
+    function testInheritingEtherAndBalancesUpdate() public {
+        vm.prank(USER);
+        oink.depositEther(5 ether);
+
+        vm.expectRevert();
+        vm.prank(BENEFICIARY);
+        oink.inheritEther(USER, 5 ether);
+
+        vm.prank(USER);
+        oink.addBeneficiary(BENEFICIARY);
+
+        vm.prank(BENEFICIARY);
+        oink.inheritEther(USER, 5 ether);
+    }
+
+    function testInheritingErc20AndBalancesUpdate() public {
+        vm.prank(USER);
+        oinkCoin.approve(address(oink), 10);
+        vm.prank(USER);
+        oink.depositErc20(OinkCoinAddress, 10);
+
+        vm.expectRevert();
+        vm.prank(BENEFICIARY);
+        oink.inheritErc20(USER, OinkCoinAddress, 10);
+
+        vm.prank(USER);
+        oink.addBeneficiary(BENEFICIARY);
+
+        vm.prank(BENEFICIARY);
+        oink.inheritErc20(USER, OinkCoinAddress, 10);
     }
 
     // function testWrittingAndRetrievingLetter() public {
